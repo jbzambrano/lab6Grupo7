6352f60a33fe115ed34ffda5dccc50816a03fabf
JZambrano <jbzambrano@pucp.pe>
2020-06-15 20:12:12 -0500

    Segundo push

--- src/main/java/sw2/lab6/teletok/controller/PostController.java
+++ src/main/java/sw2/lab6/teletok/controller/PostController.java
@@ -1,10 +1,34 @@
 package sw2.lab6.teletok.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import sw2.lab6.teletok.entity.Post;
+import sw2.lab6.teletok.entity.PostComment;
+import sw2.lab6.teletok.entity.User;
+import sw2.lab6.teletok.repository.PostCommentRepository;
+import sw2.lab6.teletok.repository.PostLikeRepository;
+import sw2.lab6.teletok.repository.PostRepository;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+import java.time.LocalDate;
 
 @Controller
 public class PostController {
+
+    @Autowired
+    PostCommentRepository postCommentRepository;
+
+    @Autowired
+    PostLikeRepository postLikeRepository;
+
+    @Autowired
+    PostRepository postRepository;
+
+
+
 
     @GetMapping(value = {"", "/"})
     public String listPost(){
@@ -17,7 +41,27 @@ public class PostController {
     }
 
     @PostMapping("/post/save")
-    public String savePost() {
+    public String savePost(@ModelAttribute("post") @Valid Post post, BindingResult bindingResult, HttpSession session) {
+
+        if(bindingResult.hasErrors()){
+
+
+            return "redirect:/post/new";
+        }
+        else {
+            User user = (User) session.getAttribute("user");
+            post.setCreationDate(LocalDate.now());
+            post.setUser(user);
+
+            if (user.getId()==0){
+                return "/post/view";
+            } else {
+                postRepository.save(post);
+                return "redirect:/";
+            }
+
+        }
+
         return "redirect:/";
     }
 
@@ -32,7 +76,10 @@ public class PostController {
     }
 
     @PostMapping("/post/comment")
-    public String postComment() {
+    public String postComment(@ModelAttribute("postComment") PostComment postComment ) {
+
+
+
         return "";
     }
 
